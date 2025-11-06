3a1adde382c6a6afc8c2ed6ee83820920c5c1a27
ROHIT SHARMA <RohitSharma.creator@gmail.com>
2025-11-06 07:15:41 +0000

    isko bhi kar do  (_for element div_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -30,7 +30,7 @@ export function Footer() {
             </div>
         </div>
       </div>
-      <div className="container py-12">
+      <div className="container py-20">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
           <div className="flex flex-col gap-4">
             <Link href="/" className="flex items-center gap-2">
