3a08f8c25a76cc9afea50903f8d982aceae59d38
ROHIT SHARMA <RohitSharma.creator@gmail.com>
2025-11-06 07:13:32 +0000

    margin add kar do left kar do (_for element h2_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -20,7 +20,7 @@ export function Footer() {
         <div className="container">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
                 <div>
-                    <h2 className="text-3xl font-bold">Become a real estate agent</h2>
+                    <h2 className="text-3xl font-bold ml-4">Become a real estate agent</h2>
                     <p>We only work with the best companies around the globe</p>
                 </div>
                 <div className="flex gap-4">
